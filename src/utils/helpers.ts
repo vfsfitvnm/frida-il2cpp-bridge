d9b2e1b1a1a122871c85ecef39ac2485a787af92
ste <maiafranco99@gmail.com>
2021-03-03 22:20:27 +0100

    Cache purposes for native-wait code

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,10 @@
+import { raise } from "./console";
+
 /** @internal */
 export const getOrNull = <T>(handle: NativePointer, target: new (handle: NativePointer) => T) =>
     handle.isNull() ? null : new target(handle);
+
+/** @internal */
+export function platformNotSupported(): never {
+    raise(`Platform "${Process.platform}" is not supported yet.`);
+}
