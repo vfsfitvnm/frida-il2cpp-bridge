37811368f6a276e474e730235669fde3773df49a
vfsfitvnm <vfsfitvnm@protonmail.com>
2022-02-13 15:21:16 +0100

    Remove Il2Cpp.Method::restoreImplementation

--- src/il2cpp/structs/method.ts
+++ src/il2cpp/structs/method.ts
@@ -146,41 +146,47 @@ class Il2CppMethod<T extends Il2Cpp.Method.ReturnType> extends NonNullNativeStru
     }
 
     /** Replaces the body of this method. */
-    set implementation(blockOrMode: "trace" | ((this: Il2Cpp.Class | Il2Cpp.Object, ...parameters: any[]) => T)) {
+    set implementation(blockOrMode: "trace" | ((this: Il2Cpp.Class | Il2Cpp.Object, ...parameters: any[]) => T) | null) {
         let replaceCallback: NativeCallbackImplementation<any, any>;
 
         const startIndex = +!this.isStatic | +Il2Cpp.unityVersionIsBelow201830;
 
-        if (blockOrMode == "trace") {
-            const offset = `0x${this.relativeVirtualAddress.toString(16).padStart(8, `0`)}`;
-            const fullName = `${this.class.type.name}.\x1b[1m${this.name}\x1b[0m`;
+        switch (blockOrMode) {
+            case null:
+                Interceptor.revert(this.virtualAddress);
+                Interceptor.flush();
+                return;
+            case "trace":
+                const offset = `0x${this.relativeVirtualAddress.toString(16).padStart(8, `0`)}`;
+                const fullName = `${this.class.type.name}.\x1b[1m${this.name}\x1b[0m`;
 
-            replaceCallback = (...args: any[]): any => {
-                const returnValue = this.nativeFunction(...args);
+                replaceCallback = (...args: any[]): any => {
+                    const returnValue = this.nativeFunction(...args);
 
-                inform(`\
+                    inform(`\
 ${offset} \
 ${fullName}\
 ${this.isStatic ? `` : `[this = ${fromFridaValue(args[0], this.class.type)}]`}\
 (${this.parameters.map((e, i) => `${e.type.name} ${e.name} = ${fromFridaValue(args[i + startIndex], e.type)}`).join(`, `)})\
 ${returnValue == undefined ? `` : ` = ${fromFridaValue(returnValue, this.returnType)}`};`);
 
-                return returnValue;
-            };
-        } else {
-            replaceCallback = (...args: any[]): any => {
-                const result = blockOrMode.call(
-                    this.isStatic ? this.class : new Il2Cpp.Object(args[0]),
-                    ...this.parameters.map((e, i) => fromFridaValue(args[i + startIndex], e.type))
-                );
+                    return returnValue;
+                };
+                break;
+            default:
+                replaceCallback = (...args: any[]): any => {
+                    const result = blockOrMode.call(
+                        this.isStatic ? this.class : new Il2Cpp.Object(args[0]),
+                        ...this.parameters.map((e, i) => fromFridaValue(args[i + startIndex], e.type))
+                    );
 
-                if (typeof result != "undefined") {
-                    return toFridaValue(result);
-                }
-            };
+                    if (typeof result != "undefined") {
+                        return toFridaValue(result);
+                    }
+                };
         }
 
-        this.restoreImplementation();
+        this.implementation = null;
 
         try {
             Interceptor.replace(this.virtualAddress, new NativeCallback(replaceCallback, this.returnType.fridaAlias, this.fridaSignature));
@@ -268,12 +274,6 @@ ${returnValue == undefined ? `` : ` = ${fromFridaValue(returnValue, this.returnT
     @levenshtein("parameters")
     parameter(name: string): Il2Cpp.Parameter {
         return this.tryParameter(name)!;
-    }
-
-    /** Restore the original method implementation. */
-    restoreImplementation(): void {
-        Interceptor.revert(this.virtualAddress);
-        Interceptor.flush();
     }
 
     /** Gets the overloaded method with the given parameter types. */
