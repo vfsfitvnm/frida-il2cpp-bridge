0ecdd4e8fa76ff0031e359a5ebc9ed2ed2f0837b
vfsfitvnm <vfsfitvnm@protonmail.com>
2022-03-20 20:46:10 +0100

    Add Il2Cpp.Reference::to

--- src/il2cpp/structs/reference.ts
+++ src/il2cpp/structs/reference.ts
@@ -1,5 +1,6 @@
 import { read, write } from "../utils";
 import { NativeStruct } from "../../utils/native-struct";
+import { raise } from "../../utils/console";
 
 /** Represent a parameter passed by reference. */
 class Il2CppReference<T extends Il2Cpp.Field.Type> extends NativeStruct {
@@ -20,6 +21,64 @@ class Il2CppReference<T extends Il2Cpp.Field.Type> extends NativeStruct {
     /** */
     toString(): string {
         return this.isNull() ? "null" : `->${this.value}`;
+    }
+
+    static to<T extends number | NativePointer>(value: T, type: Il2Cpp.Type): Il2Cpp.Reference<T>;
+
+    static to<T extends Exclude<Il2Cpp.Field.Type, number | NativePointer>>(value: T): Il2Cpp.Reference<T>;
+
+    /** Creates a reference to the specified value. */
+    static to<T extends Il2Cpp.Field.Type>(value: T, type?: Il2Cpp.Type): Il2Cpp.Reference<T> {
+        const handle = Memory.alloc(Process.pointerSize);
+
+        switch (typeof value) {
+            case "boolean":
+                return new Il2Cpp.Reference(handle.writeS8(+value), Il2Cpp.Image.corlib.class("System.Boolean").type);
+            case "number":
+                switch (type?.typeEnum) {
+                    case Il2Cpp.Type.Enum.U1:
+                        return new Il2Cpp.Reference<T>(handle.writeU8(value), type);
+                    case Il2Cpp.Type.Enum.I1:
+                        return new Il2Cpp.Reference<T>(handle.writeS8(value), type);
+                    case Il2Cpp.Type.Enum.Char:
+                    case Il2Cpp.Type.Enum.U2:
+                        return new Il2Cpp.Reference<T>(handle.writeU16(value), type);
+                    case Il2Cpp.Type.Enum.I2:
+                        return new Il2Cpp.Reference<T>(handle.writeS16(value), type);
+                    case Il2Cpp.Type.Enum.U4:
+                        return new Il2Cpp.Reference<T>(handle.writeU32(value), type);
+                    case Il2Cpp.Type.Enum.I4:
+                        return new Il2Cpp.Reference<T>(handle.writeS32(value), type);
+                    case Il2Cpp.Type.Enum.U8:
+                        return new Il2Cpp.Reference<T>(handle.writeU64(value), type);
+                    case Il2Cpp.Type.Enum.I8:
+                        return new Il2Cpp.Reference<T>(handle.writeS64(value), type);
+                    case Il2Cpp.Type.Enum.R4:
+                        return new Il2Cpp.Reference<T>(handle.writeFloat(value), type);
+                    case Il2Cpp.Type.Enum.R8:
+                        return new Il2Cpp.Reference<T>(handle.writeDouble(value), type);
+                    }
+            case "object":
+                if (value instanceof Il2Cpp.ValueType || value instanceof Il2Cpp.Pointer) {
+                    return new Il2Cpp.Reference<T>(handle.writePointer(value), value.type);
+                } else if (value instanceof Il2Cpp.Object) {
+                    return new Il2Cpp.Reference<T>(handle.writePointer(value), value.class.type);
+                } else if (value instanceof Il2Cpp.String || value instanceof Il2Cpp.Array) {
+                    return new Il2Cpp.Reference<T>(handle.writePointer(value), value.object.class.type);
+                } else if (value instanceof NativePointer) {
+                    switch (type?.typeEnum) {
+                        case Il2Cpp.Type.Enum.UnsignedNativeInteger:
+                        case Il2Cpp.Type.Enum.NativeInteger:
+                            return new Il2Cpp.Reference<T>(handle.writePointer(value), type);
+                    }
+                } else if (value instanceof Int64) {
+                    return new Il2Cpp.Reference<T>(handle.writeS64(value), Il2Cpp.Image.corlib.class("System.Int64").type);
+                } else if (value instanceof UInt64) {
+                    return new Il2Cpp.Reference<T>(handle.writeU64(value), Il2Cpp.Image.corlib.class("System.UInt64").type);
+                }
+            default:
+                raise(`don't know how to create a reference to ${value} using type ${type?.name}`);
+        }
     }
 }
 
