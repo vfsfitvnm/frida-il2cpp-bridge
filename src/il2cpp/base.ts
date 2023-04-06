9ec11e9e23d98ef42100f04c4dc9ba0b78c95ce0
vfsfitvnm <vfsfitvnm@protonmail.com>
2023-04-06 20:48:21 +0200

    Compare thread pointer in Il2Cpp::installExceptionListener

--- src/il2cpp/base.ts
+++ src/il2cpp/base.ts
@@ -184,10 +184,10 @@ class Il2CppBase {
 
     /** */
     static installExceptionListener(targetThread: "current" | "all" = "current"): InvocationListener {
-        const threadId = Process.getCurrentThreadId();
+        const currentThread = Il2Cpp.Api._threadCurrent();
 
         return Interceptor.attach(Il2Cpp.module.getExportByName("__cxa_throw"), function (args) {
-            if (targetThread == "current" && this.threadId != threadId) {
+            if (targetThread == "current" && !Il2Cpp.Api._threadCurrent().equals(currentThread)) {
                 return;
             }
 
