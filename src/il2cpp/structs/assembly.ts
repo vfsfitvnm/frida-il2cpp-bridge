56fe0f04347e2a5918ecd2ef59fa56845cea5826
vfsfitvnm <vfsfitvnm@protonmail.com>
2023-05-22 21:09:22 +0200

    Add fallback for missing il2cpp_assembly_get_image (#300)

--- src/il2cpp/structs/assembly.ts
+++ src/il2cpp/structs/assembly.ts
@@ -2,9 +2,38 @@ namespace Il2Cpp {
     @recycle
     export class Assembly extends NativeStruct {
         /** Gets the image of this assembly. */
-        @lazy
         get image(): Il2Cpp.Image {
-            return new Il2Cpp.Image(Il2Cpp.api.assemblyGetImage(this));
+            let get = function (this: Il2Cpp.Assembly) {
+                return new Il2Cpp.Image(Il2Cpp.api.assemblyGetImage(this));
+            };
+
+            try {
+                Il2Cpp.api.assemblyGetImage;
+            } catch (_) {
+                get = function (this: Il2Cpp.Assembly) {
+                    // We need to get the System.Reflection.Module of the current assembly;
+                    // System.Reflection.Assembly::GetModulesInternal, for some reason,
+                    // throws a NullReferenceExceptionin Unity 5.3.8f1, so we must rely on
+                    // System.Type::get_Module instead.
+                    // Now we need to get any System.Type of this assembly.
+                    // We cannot use System.Reflection.Assembly::GetTypes because it may
+                    // return an empty array; hence we use System.Reflection.Assembly::GetType
+                    // to retrieve <Module>, a class/type that seems to be always present
+                    // (despite being excluded from System.Reflection.Assembly::GetTypes).
+                    return new Il2Cpp.Image(
+                        this.object
+                            .method<Il2Cpp.Object>("GetType", 1)
+                            .invoke(Il2Cpp.string("<Module>"))
+                            .method<Il2Cpp.Object>("get_Module")
+                            .invoke()
+                            .field<NativePointer>("_impl").value
+                    );
+                };
+            }
+
+            getter(Il2Cpp.Assembly.prototype, "image", get, lazy);
+
+            return this.image;
         }
 
         /** Gets the name of this assembly. */
