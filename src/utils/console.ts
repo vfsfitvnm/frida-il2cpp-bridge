67fb07e5641d85b0b915f3f87f6c67eb6efb6e8d
vfsfitvnm <vfsfitvnm@protonmail.com>
2023-09-08 15:42:53 +0200

    Lowercase ansi color code chars

--- src/utils/console.ts
+++ src/utils/console.ts
@@ -1,7 +1,7 @@
 /** @internal */
 function raise(message: any): never {
-    const error = new Error(`\x1B[0m${message}`);
-    error.name = `\x1B[0m\x1B[38;5;9mil2cpp\x1B[0m`;
+    const error = new Error(`\x1b[0m${message}`);
+    error.name = `\x1b[0m\x1b[38;5;9mil2cpp\x1b[0m`;
     error.stack = error.stack
         ?.replace(/^Error/, error.name)
         ?.replace(/\n    at (.+) \((.+):(.+)\)/, "\x1b[3m\x1b[2m")
@@ -12,15 +12,15 @@ function raise(message: any): never {
 
 /** @internal */
 function warn(message: any): void {
-    (globalThis as any).console.log(`\x1B[38;5;11mil2cpp\x1B[0m: ${message}`);
+    (globalThis as any).console.log(`\x1b[38;5;11mil2cpp\x1b[0m: ${message}`);
 }
 
 /** @internal */
 function ok(message: any): void {
-    (globalThis as any).console.log(`\x1B[38;5;10mil2cpp\x1B[0m: ${message}`);
+    (globalThis as any).console.log(`\x1b[38;5;10mil2cpp\x1b[0m: ${message}`);
 }
 
 /** @internal */
 function inform(message: any): void {
-    (globalThis as any).console.log(`\x1B[38;5;12mil2cpp\x1B[0m: ${message}`);
+    (globalThis as any).console.log(`\x1b[38;5;12mil2cpp\x1b[0m: ${message}`);
 }
