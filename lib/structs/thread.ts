319bb9334e2c8d27c8caa0b8ed19bd9273ba2497
vfsfitvnm <vfsfitvnm@protonmail.com>
2026-08-30 14:49:10 +0200

    lib: Tweak Il2Cpp::attachedThreads fallback performance

--- lib/structs/thread.ts
+++ lib/structs/thread.ts
@@ -130,34 +130,58 @@ namespace Il2Cpp {
         }
     }
 
-    /** Gets the attached threads. */
+    /** Gets the threads attached to IL2CPP. */
     export declare const attachedThreads: Il2Cpp.Thread[];
     getter(Il2Cpp, "attachedThreads", () => {
         if (Il2Cpp.exports.threadGetAttachedThreads.isNull()) {
-            const currentThreadHandle = Il2Cpp.currentThread?.handle ?? raise("Current thread is not attached to IL2CPP");
+            // attached threads are stored in s_AttachedThreads (a std::vector in libil2cpp/vm/Thread.cpp)
+            // in case il2cpp_thread_get_all_attached_threads is not available, we can scan the
+            // memory to look for the current thread pointer, so that we get where s_AttachedThreads
+            // is contiguously storing its values (pointers to threads)
+            const currentThreadHandle = Il2Cpp.currentThread?.handle ?? raise("current thread is not attached to IL2CPP");
             const pattern = currentThreadHandle.toMatchPattern();
 
-            const threads: Il2Cpp.Thread[] = [];
+            // s_AttachedThreads is initialized at runtime (il2cpp_init) and it is managed by IL2CPP GC
+            const rangeProvider = function* () {
+                // ranges where other static data allocated during il2cpp_init is
+                yield Process.getRangeByAddress(Il2Cpp.corlib.assembly.object);
+                yield Process.getRangeByAddress(Il2Cpp.corlib.class("System.Threading.Thread").staticFieldsData);
 
-            for (const range of Process.enumerateRanges("rw-")) {
-                if (range.file == undefined) {
-                    const matches = Memory.scanSync(range.base, range.size, pattern);
-                    if (matches.length == 1) {
-                        while (true) {
-                            const handle = matches[0].address.sub(matches[0].size * threads.length).readPointer();
+                // ranges allocated by IL2CPP garbage collector
+                try {
+                    yield* Il2Cpp.gc.heapSections;
+                } catch (_) {} // ignore missing export or native exceptions
 
-                            if (handle.isNull() || !handle.readPointer().equals(currentThreadHandle.readPointer())) {
-                                break;
-                            }
+                // last resort: every range in the whole process that is likely to be heap allocated
+                yield* Process.enumerateRanges("rw-").filter(_ => _.file == undefined);
+            };
 
-                            threads.unshift(new Il2Cpp.Thread(handle));
-                        }
-                        break;
+            for (const range of rangeProvider()) {
+                let matches: MemoryScanMatch[] = [];
+                try {
+                    // TODO: replace with Memory.findPointers once it's stable
+                    matches = Memory.scanSync(range.base, range.size, pattern);
+                } catch (_) {} // ignore access violation errors (https://github.com/vfsfitvnm/frida-il2cpp-bridge/issues/742)
+
+                if (matches.length == 1) {
+                    // we could be at the middle of the vector, so let's go to its head
+                    let { address } = matches[0];
+                    while (!address.sub(Process.pointerSize).readPointer().isNull()) {
+                        address = address.sub(Process.pointerSize);
                     }
+
+                    const threads: Il2Cpp.Thread[] = [];
+
+                    let handle: NativePointer;
+                    while (!(handle = address.add(Process.pointerSize * threads.length).readPointer()).isNull()) {
+                        threads.push(new Il2Cpp.Thread(handle));
+                    }
+
+                    return threads;
                 }
             }
 
-            return threads;
+            raise("couldn't collect attached threads");
         }
 
         return readNativeList(Il2Cpp.exports.threadGetAttachedThreads).map(_ => new Il2Cpp.Thread(_));
